ba21d54194823d140d5ed71e06414d02b2e7011e
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-03 14:40:34 -0800

    Update Freeze.php

--- src/Terpz710/ModerationPE/Commands/Freeze.php
+++ src/Terpz710/ModerationPE/Commands/Freeze.php
@@ -4,7 +4,7 @@ namespace Terpz710\ModerationPE\Commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-use pocketmine\player\Player;
+use pocketmine\entity\Entity;
 use pocketmine\Server;
 
 use Terpz710\ModerationPE\Staff;
@@ -24,12 +24,12 @@ class Freeze extends Command
         if (!Staff::getInstance()->hasPermissionPlayer($sender, "freeze")) return;
 
         if (isset($args[0])) {
-            $player = Server::getInstance()->getPlayerByPrefix($args[0]);
-            if ($player instanceof Player) {
-                if (!$player->hasNoClientPredictions()) {
-                    $player->setNoClientPredictions();
-                    $sender->sendMessage(Staff::getInstance()->getConfigReplace("freeze_sender", "{player}", $player->getName()));
-                    $player->sendMessage(Staff::getInstance()->getConfigReplace("freeze_player", "{player}", $sender->getName()));
+            $entity = Server::getInstance()->getPlayerByPrefix($args[0]);
+            if ($entity instanceof Entity) {
+                if (!$entity->hasNoClientPredictions()) {
+                    $entity->setNoClientPredictions();
+                    $entity->sendMessage(Staff::getInstance()->getConfigReplace("freeze_sender", "{player}", $player->getName()));
+                    $entity->sendMessage(Staff::getInstance()->getConfigReplace("freeze_player", "{player}", $sender->getName()));
                     Staff::getInstance()->sendDiscordMessage($player, $sender, null, "freeze_discord");
                 } else $sender->sendMessage(Staff::getInstance()->getConfigReplace("freeze_freeze"));
             } else $sender->sendMessage(Staff::getInstance()->getConfigReplace("no_online_player"));
